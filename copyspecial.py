a9fbad0aafc79ae381c4c87b05fa46b1f8e30124
Nikal Morgan <nikalmorgan@Nikals-Air.attlocal.net>
2020-06-13 16:06:21 -0500

    complete get_special_paths

--- copyspecial.py
+++ copyspecial.py
@@ -7,7 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 
 # give credits
-__author__ = "???"
+__author__ = "Nikal Morgan"
 
 import re
 import os
@@ -19,8 +19,15 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
-    # your code here
-    return
+    specail_dir_files = []
+    for file in os.listdir(dirname):
+        # print(file)
+        special_file = re.findall(r'__(\w+)__', file)
+        # print(special_file)
+        if special_file:
+            specail_dir_files.append(os.path.abspath(file)) #os.path.abspath gets the absolute path for file
+    # print(specail_dir_files)
+    return specail_dir_files
 
 
 def copy_to(path_list, dest_dir):
@@ -36,10 +43,11 @@ def zip_to(path_list, dest_zip):
 def main(args):
     """Main driver code for copyspecial."""
     # This snippet will help you get started with the argparse module.
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--todir', help='dest dir for special files')
+    parser = argparse.ArgumentParser() # -- or - is optional but with out is required or positional
+    parser.add_argument('--todir', help='dest dir for special files') 
     parser.add_argument('--tozip', help='dest zipfile for special files')
     # TODO: add one more argument definition to parse the 'from_dir' argument
+    parser.add_argument('from_dir', help='find dir for special files')
     ns = parser.parse_args(args)
 
     # TODO: you must write your own code to get the command line args.
@@ -50,7 +58,10 @@ def main(args):
     # any required args, the general rule is to print a usage message and
     # exit(1).
 
-    # Your code here: Invoke (call) your functions
+    if len(sys.argv) < 1:
+        parser.print_usage()
+    get_special_paths(ns.from_dir) #ns.from_dir this get files in directory given
+
 
 
 if __name__ == "__main__":
